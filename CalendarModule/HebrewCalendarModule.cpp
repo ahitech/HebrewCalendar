f3983e79c1d2e146c559b2c939baf89c6f0efe15
Alex Hitech <ahitech@gmail.com>
2020-02-26 01:51:27 +0200

    More month names.

--- CalendarModule/HebrewCalendarModule.cpp
+++ CalendarModule/HebrewCalendarModule.cpp
@@ -36,7 +36,57 @@ HebrewCalendar::HebrewCalendar (void)
 	names.shortName = BString("Kheshvan");
 	this->fMonthNames[2] = names;
 
-
+	names.longName = BString ("Kislev");
+	names.shortName = BString ("Kislev");
+	this->fMonthNames[3] = names;
+	
+	names.longName = BString("Tevet");
+	names.shortName = BString ("Tevet");
+	this->fMonthNames[4] = names;
+	
+	names.longName = BString("Shevat");
+	names.shortName = BString ("Shevat");
+	this->fMonthNames[5] = names;
+	
+	// Actually, the Adar should be omitted; libhdate should never
+	// return 6, returning 13 or 14 instead (see below).
+	// I leave it here because holes in arrays are bad practice.
+	names.longName = BString("Adar")
+	names.shortName = BString ("Adar");
+	this->fMonthNames[6] = names;
+	
+	names.longName = BString("Nisan");
+	names.shortName = BString("Nisan");
+	this->fMonthNames[7] = names;
+	
+	names.longName = BString("Iyar");
+	names.shortName = BString("Iyar");
+	this->fMonthNames[8] = names;
+	
+	names.longName = BString ("Sivan");
+	names.shortName = BString ("Sivan");
+	this->fMonthNames[9] = names;
+	
+	names.longName = BString ("Tammuz");
+	names.shortName = BString ("Tammuz");
+	this->fMonthNames[10] = names;
+	
+	names.longName = BString ("Av");
+	names.shortName = BString ("Av");
+	this->fMonthNames[11] = names;
+	
+	names.longName = BString ("Elul");
+	names.shortName = BString ("Elul");
+	this->fMonthNames[12] = names;
+	
+	names.longName = BString("Adar")
+	names.shortName = BString ("Adar");
+	this->fMonthNames[13] = names;
+	
+	names.longName = BString("Adar B")
+	names.shortName = BString ("Adar B");
+	this->fMonthNames[14] = names;
+	
 	// Fill in the names of the weekdays
 }
 
